56c4ac92c536e9e976fb85969cb0864029b95b8d
kamatanikyouhei <kamatanikyouhei@kamatanikyouheinoMacBook-ea.local>
2018-12-04 14:17:55 +0900

    課題修正3

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,21 @@
 class ArticlesController < ApplicationController
+ def   show
+    @articles = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @articles = Article.new
+  end
+
+
+  private
+
+  def article_params
+    params.require(:Article).permit(:title, :body,)
+  end
+
 end
--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -5,23 +5,6 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 
-  def   show
-    @articles = Article.find(params[:id])
-  end
-
-  def index
-    @articles = Article.all
-  end
-
-  def new
-    @articles = Article.new
-  end
-
-
-  private
-
-  def article_params
-    params.require(:item).permit(:title, :body,)
-  end
+ 
 
 end
